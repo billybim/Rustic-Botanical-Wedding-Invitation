0fdc137c73322bb670cba7a96bd2d9be7eb3f408
Billy Cessar Bimara <billyexsa@gmail.com>
2026-07-18 09:12:19 +0700

    setting config for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/Rustic-Botanical-Wedding-Invitation/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
